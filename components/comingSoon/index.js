2c16141974d1034e7534c6d69cdd1c1da956d00b
Jamieson Roberts <hello@jamiesonroberts.com>
2019-12-06 09:25:30 -0500

    Adding in email address

--- components/comingSoon/index.js
+++ components/comingSoon/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faInstagram, faRavelry } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope } from '@fortawesome/free-regular-svg-icons';
 
 import styles from './styles.css';
 import Logo from '../logo';
@@ -20,8 +21,13 @@ export default () => (
                     <FontAwesomeIcon icon={faRavelry}/>
                     <span className={styles.invisible}>Follow Rowan & Raven Yarn on Ravelry</span>
                 </a>
+                <a href="mailto:info@rowanandravenyarn.ca" className={styles.socialLink}>
+                    <FontAwesomeIcon icon={faEnvelope}/>
+                    <span className={styles.invisible}>Email Rowan & Raven Yarn</span>
+                </a>
             </div>
             <p>Coming soon</p>
+            <p><a href="mailto:info@rowanandravenyarn.ca">info@rowanandravenyarn.ca</a></p>
         </div>
     </main>
 )
