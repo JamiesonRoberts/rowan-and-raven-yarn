2715ced820d5c0547d072d7eac445fcebb827230
Jamieson Roberts <hello@jamiesonroberts.com>
2024-01-08 09:45:18 -0500

    Updating repo files

---
+++ README.md
@@ -0,0 +1,37 @@
+# Rowan & Raven Yarn
+
+## 📦 Install
+
+```
+$ git clone https://github.com/JamiesonRoberts/rowan-and-raven-yarn.git
+
+$ nvm i
+
+$ npm i
+```
+
+## 🔨 Building
+
+- Runs next which starts Next.js in development mode
+
+```bash
+$ npm run dev
+```
+
+- Runs next build which builds the application for production usage
+
+```bash
+$ npm run build
+```
+
+- Runs next start which starts a Next.js production server
+
+```bash
+$ npm run start
+```
+
+- Runs next lint which checks for potential issues
+
+```bash
+$ npm run lint
+```
--- pages/_app.js
+++ pages/_app.js
@@ -1,69 +1,58 @@
 import Head from 'next/head'
-import PlausibleProvider from 'next-plausible'
 import { Raleway, Roboto } from 'next/font/google'
 
 import '@/styles/styles.css'
 
 const robotoFont = Roboto({
-    subsets: ['latin'],
-    weight: '400',
-    fallback: ['Helvetica Neue', 'Helvetica', 'sans-serif'],
-    display: 'swap',
+  subsets: ['latin'],
+  weight: '400',
+  fallback: ['Helvetica Neue', 'Helvetica', 'sans-serif'],
+  display: 'swap',
 })
 
 const ralewayFont = Raleway({
-    subsets: ['latin'],
-    fallback: ['Helvetica Neue', 'Helvetica', 'sans-serif'],
-    display: 'swap',
+  subsets: ['latin'],
+  fallback: ['Helvetica Neue', 'Helvetica', 'sans-serif'],
+  display: 'swap',
 })
 
 const App = ({ Component, pageProps }) => {
-    return (
-        <PlausibleProvider
-            domain='rowanandravenyarn.ca'
-            trackOutboundLinks={true}
-        >
-            <Head>
-                <meta
-                    name='viewport'
-                    content='width=device-width, initial-scale=1'
-                />
-                <link rel='shortcut icon' href='/favicon.ico' />
-                <link
-                    rel='apple-touch-icon'
-                    sizes='180x180'
-                    href='/apple-touch-icon.png'
-                />
-                <link
-                    rel='icon'
-                    type='image/png'
-                    sizes='32x32'
-                    href='/favicon-32x32.png'
-                />
-                <link
-                    rel='icon'
-                    type='image/png'
-                    sizes='16x16'
-                    href='/favicon-16x16.png'
-                />
-                <link rel='manifest' href='/site.webmanifest' />
-                <link
-                    rel='mask-icon'
-                    href='/safari-pinned-tab.svg'
-                    color='#000000'
-                />
-                <meta name='msapplication-TileColor' content='#da532c' />
-                <meta name='theme-color' content='#ffffff' />
-            </Head>
-            <style jsx global>{`
-                :root {
-                    --primary-font: ${robotoFont.style.fontFamily};
-                    --heading-font: ${ralewayFont.style.fontFamily};
-                }
-            `}</style>
-            <Component {...pageProps} />
-        </PlausibleProvider>
-    )
+  return (
+    <>
+      <Head>
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
+        <link rel='shortcut icon' href='/favicon.ico' />
+        <link
+          rel='apple-touch-icon'
+          sizes='180x180'
+          href='/apple-touch-icon.png'
+        />
+        <link
+          rel='icon'
+          type='image/png'
+          sizes='32x32'
+          href='/favicon-32x32.png'
+        />
+        <link
+          rel='icon'
+          type='image/png'
+          sizes='16x16'
+          href='/favicon-16x16.png'
+        />
+        <link rel='manifest' href='/site.webmanifest' />
+        <link rel='mask-icon' href='/safari-pinned-tab.svg' color='#000000' />
+        <meta name='msapplication-TileColor' content='#da532c' />
+        <meta name='theme-color' content='#ffffff' />
+      </Head>
+      <style jsx global>{`
+        :root {
+          --primary-font: ${robotoFont.style.fontFamily};
+          --heading-font: ${ralewayFont.style.fontFamily};
+        }
+      `}</style>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default App
